353f1bcce778502a960e33b5922d7c9a70931473
fernando-sebastiao <fernandosebastiao888@gmail.com>
2025-08-14 10:57:23 +0100

    adicionado swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Configurando o Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Api de Livros')
+    .setDescription('Documentação da API de livros com NestJs + Swagger')
+    .setVersion('1.0')
+    .addTag('book')
+    .build();
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Server runing on PORT: ${process.env.PORT ?? 3000}`);
 }
 bootstrap();
