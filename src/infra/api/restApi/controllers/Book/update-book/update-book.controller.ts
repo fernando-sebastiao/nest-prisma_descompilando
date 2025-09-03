3eb7d92a012f9f1f3957863da8908906639e87f0
fernando-sebastiao <fernandosebastiao888@gmail.com>
2025-09-03 21:27:59 +0100

    feito a rota de actualizar book

---
+++ src/infra/api/restApi/controllers/Book/update-book/update-book.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Param, Put } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Book } from 'src/domain/entities/book-entity';
+import { UpdateBookService } from 'src/usecases/Book/update-book/update-book.service';
+import { updateBookDTO } from 'src/usecases/dtos/update-book-input';
+
+@ApiTags('Book')
+@Controller('update-book')
+export class UpdateBookController {
+  constructor(private readonly updateBookService: UpdateBookService) {}
+  @ApiOperation({ description: 'Actualiza um livro!' })
+  @ApiResponse({
+    status: 200,
+    description: 'Livro atualizado com sucesso!',
+    type: Book,
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Livro não encontrado!',
+    type: Book,
+  })
+  @Put(':bookId')
+  async updateBook(
+    @Body() data: updateBookDTO,
+    @Param('bookId') bookId: string,
+  ) {
+    return await this.updateBookService.execute(bookId, data);
+  }
+}
