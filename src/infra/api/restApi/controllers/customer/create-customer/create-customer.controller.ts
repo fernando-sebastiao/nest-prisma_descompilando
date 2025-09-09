7268c92c51901fc9e0c3863837df288c45b9b527
fernando-sebastiao <fernandosebastiao888@gmail.com>
2025-09-09 20:14:36 +0100

    feito a rota para criar um customer(cliente)

---
+++ src/infra/api/restApi/controllers/customer/create-customer/create-customer.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CustomerEntity } from 'src/domain/entities/customer-entity';
+import { CreateCustomerService } from 'src/usecases/customer/create-customer/create-customer.service';
+import { CustomerDTO } from 'src/usecases/dtos/custumerInputs';
+
+@ApiTags('Customer')
+@Controller('custumer')
+export class CreateCustomerController {
+  constructor(private readonly createCustomerService: CreateCustomerService) {}
+
+  @ApiOperation({ description: 'criar um cliente(customer)' })
+  @ApiResponse({
+    description: 'cliente(customer) criado com sucesso',
+    status: 201,
+    type: CustomerEntity,
+  })
+  @ApiResponse({
+    description: 'cliente(customer) já existe! Verifique os dados',
+    status: 401,
+  })
+  @Post()
+  async createCustomer(@Body() data: CustomerDTO): Promise<CustomerEntity> {
+    return await this.createCustomerService.execute(data);
+  }
+}
