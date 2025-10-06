a552bce5bf1b2eecc33ef0f76c7ddd3e8dcaf75c
fernando-sebastiao <fernandosebastiao888@gmail.com>
2025-10-06 10:03:28 +0100

    adicionado index no schema.prisma

---
+++ prisma/migrations/20251006090218_/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[phone]` on the table `customers` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
+CREATE UNIQUE INDEX "customers_phone_key" ON "customers"("phone");
+
+-- CreateIndex
+CREATE INDEX "sale_items_bookId_customerId_idx" ON "sale_items"("bookId", "customerId");
